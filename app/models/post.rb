e56c28d422593d1919daffb2aee0a6b7cb8785c7
Prelang Builder <builder@prelang.com>
2016-04-03 21:34:22 +0000

    Defined validation for attribute 'video_url' on model 'Post'

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   belongs_to :project
   validates_formatting_of :image_url, using: :url
   validates_formatting_of :music_url, using: :url
+  validates_formatting_of :video_url, using: :url
 end
